dffa9ff7282fb58b03f8da4a022a8046cfda71be
AndreySHSH <laptev.andrey@icloud.com>
2022-08-23 17:20:11 +0300

    add auth

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,10 @@
-import React, {Component} from "react";
+import React, {Component, Suspense} from "react";
 import Header from "./header";
-import Content from "./content";
 import Footer from "./footer";
+import {NotFoundBoundary, Router, View} from 'react-navi'
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {mount, route} from 'navi'
+import Feed from "./feed";
 
 class App extends Component{
     constructor(props) {
@@ -9,6 +12,11 @@ class App extends Component{
         this.state = {
 
         }
+
+        this.routes = mount({
+            '/' : route({view: <Feed/>}),
+
+        })
     }
 
     render() {
@@ -16,8 +24,45 @@ class App extends Component{
             <>
                 <Header auth={false} load={true}/>
                 <div className="wrapper">
-                    <Content/>
-                    <Footer/>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route path="/web/"
+                                   render={({history, match}) => <Router routes={this.routes} history={history}
+                                                                         basename={match.url}>
+
+                                       {/*<ToastContainer*/}
+                                       {/*    position="bottom-right"*/}
+                                       {/*    autoClose={2000}*/}
+                                       {/*    hideProgressBar={false}*/}
+                                       {/*    newestOnTop={false}*/}
+                                       {/*    closeOnClick*/}
+                                       {/*    rtl={false}*/}
+                                       {/*    pauseOnFocusLoss*/}
+                                       {/*    draggable*/}
+                                       {/*    pauseOnHover*/}
+                                       {/*/>*/}
+
+
+                                       <NotFoundBoundary
+                                           render={() =>
+                                           <div className="wrapper-feed">
+                                               <div className="wrapper-error">
+                                                   <div className="error">404 Такой страницы не существует</div>
+                                                   <div className="error small-text">вернуться <div
+                                                       style={{textDecoration: "underline", cursor: "pointer"}}
+                                                       onClick={() => {window.history.go(-1)}}>Назад</div></div>
+                                               </div>
+                                           </div>}>
+                                           <Suspense fallback={true}>
+                                               <View />
+                                           </Suspense>
+                                       </NotFoundBoundary>
+                                       <Footer/>
+                                   </Router>
+
+                               }/>
+                        </Switch>
+                    </BrowserRouter>
                 </div>
             </>
         )
--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,76 @@ class Header extends Component{
         super(props);
         this.state = {
             isAuth: props.auth,
-            isLoad: props.load
+            isLoad: props.load,
+            targetAction: "sign-in",
+            isAuthOpen: false,
         }
+    }
+
+    swapButton = (e) => {
+        this.setState({targetAction: e.target.getAttribute("target")})
+
+        let childrenCollection = e.target.parentNode.children
+
+        for (let i=0; i !== childrenCollection.length; i++) {
+            e.target.parentNode.children[i].classList.remove('active')
+        }
+        e.target.classList.add("active")
+    }
+
+    openAuth = () => {
+        this.setState({isAuthOpen: true})
     }
 
     render() {
         return (
             <div className="header-bg">
+                {
+                    this.state.isAuthOpen ?
+                        <div className="wrapper-pop-up">
+                            <div className="wrapper-auth-pop-up">
+                                {/*<div className="title-pop-up">*/}
+                                {/*    Войти*/}
+                                {/*</div>*/}
+                                <div className="control-pop-up">
+                                    <div className="control-pop-up-item active unselectable" target="sign-in" onClick={this.swapButton}>Войти</div>
+                                    <div className="control-pop-up-item unselectable" target="sign-up" onClick={this.swapButton}>Регистрация</div>
+                                </div>
+                                {
+                                    this.state.targetAction === "sign-in" ?
+                                        <div className="sign-in-place">
+                                            <div className="wrapper-input">
+                                                <input className="input-default" placeholder="Логин / Номер телефона"/>
+                                            </div>
+                                            <div className="wrapper-input">
+                                                <input className="input-default" type="password" placeholder="Пароль"/>
+                                            </div>
+                                            <div className="wrapper-input-sign">
+                                                <div className="button-sign unselectable">Войти</div>
+                                            </div>
+                                        </div>
+                                        :
+                                        <div className="sign-in-place">
+                                            <div className="wrapper-input">
+                                                <input className="input-default" placeholder="Логин"/>
+                                            </div>
+                                            <div className="wrapper-input">
+                                                <input className="input-default" type="password" placeholder="Пароль"/>
+                                            </div>
+                                            <div className="wrapper-input">
+                                                <input className="input-default" type="password" placeholder="Повторите пароль"/>
+                                            </div>
+                                            <div className="wrapper-input-sign">
+                                                <div className="button-sign unselectable">Регистрация</div>
+                                            </div>
+                                        </div>
+                                }
+
+                            </div>
+                        </div>
+                    :
+                        null
+                }
                 <div className="wrapper-header">
                     <div className="place-logo" onClick={(e) => {
                         e.preventDefault();
@@ -30,7 +93,7 @@ class Header extends Component{
                                 this.state.isAuth ?
                                     <div className="">test</div>
                                     :
-                                    <div className="button-default unselectable">Войти / Регистрация</div>
+                                    <div className="button-default unselectable" onClick={this.openAuth}>Войти / Регистрация</div>
                                 :
                             <div className="loader-small"/>
                         }
